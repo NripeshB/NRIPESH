ebe3311c1268fbc2a01b62e4d047cac206ba2875
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-06-01 15:12:06 +0530

    .

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-
+# Set the page configuration (optional)
 st.set_page_config(page_title="Happy Birthday Nripesh", layout="centered")
 
 # Main title and subtitle
-st.title("Happy Birthday Nripesh!")
+st.title("Happy Birthday Nripesh!!")
 st.subheader("🎉🎉✨✨")
 
 # One Piece quotes to complement the celebration
